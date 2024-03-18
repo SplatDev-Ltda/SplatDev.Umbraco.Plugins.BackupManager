131fbc0a4e83a4c1b22e29e4dc785cdfccc4f14f
Shuchita <ariel.shuchita@gmail.com>
2024-03-18 16:37:49 +0530

    Rename files for Version 13

--- v13.2.0/src/v13.2.0.bat
+++ v13.2.0/src/v13.2.0.bat
@@ -1,6 +1,6 @@
-dotnet new install Umbraco.Templates::13.1.0
+dotnet new install Umbraco.Templates::13.2.0
 # Create solution/project
-dotnet new sln --name "SplatDev.Umbraco.Plugin.v13.1.0"
+dotnet new sln --name "SplatDev.Umbraco.Plugin.v13.2.0"
 dotnet new umbraco --force -n "SplatDev.Umbraco.Web"
 dotnet sln add "SplatDev.Umbraco.Web"
 dotnet new umbracopackage --force -n "SplatDev.Umbraco.Plugin.Backups"
